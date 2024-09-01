024a57e12878c509943497e517efbf04855c69f6
Maxeu444 <139349788+Maxeu444@users.noreply.github.com>
2024-09-01 20:20:45 +0200

    add User model and start register route

---
+++ todolist/app/api/user/auth/register.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
+export const POST = async (request: Request) => {
+    
+} 
